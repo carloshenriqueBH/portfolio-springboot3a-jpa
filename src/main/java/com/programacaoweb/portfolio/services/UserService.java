7a4d0b871f3f4118c062380fdd765114e5159f4d
Carlos Henrique <carlhenrique@gmail.com>
2024-04-12 16:30:16 -0300

    Service Layer, component registration

---
+++ src/main/java/com/programacaoweb/portfolio/services/UserService.java
@@ -0,0 +1,30 @@
+package com.programacaoweb.portfolio.services;
+
+import java.util.List;
+import java.util.Optional;
+
+//import org.apache.el.stream.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.programacaoweb.portfolio.entities.User;
+import com.programacaoweb.portfolio.repositories.UserRepository;
+
+@Service
+public class UserService {
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+	
+
+}
